d8956c5a7b9a274e440ad87eb5404dc66026d6ed
Pedro <projasma2002@gmail.com>
2026-08-16 00:32:27 -0500

    FEAT: Configuracion Parte II Eureka Gateway Resilience4j y reglas de cuenta

---
+++ src/test/java/com/bank/config/ConfigServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.bank.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ConfigServerApplicationTest {
+
+    @Test
+    void applicationClassIsPresent() {
+        assertNotNull(ConfigServerApplication.class);
+    }
+}
